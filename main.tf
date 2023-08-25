6cf03ba23a10098590a80ed42c671b81b08ec492
amol-gitlab <amol@yourdomain.com>
2023-08-26 01:29:07 +0530

    fsfsfsfffd

--- main.tf
+++ main.tf
@@ -8,9 +8,7 @@ provider "aws" {
 resource "aws_instance" "Demo" {
   ami           = "ami-08a52ddb321b32a8c"
   instance_type = "t2.micro"
-  key_name = "key_pair_name"
-  tags = {
-    Name = "Demo"
+  
   }
-}
+
 ###########ssssss
