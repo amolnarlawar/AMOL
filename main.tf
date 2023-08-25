b31bd4ac54bdf81730b0b791abd7db28fb97b2f1
amol-gitlab <amol@yourdomain.com>
2023-08-26 01:32:03 +0530

    fsf

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 provider "aws" {
   access_key = "AKIASY5UYM73KVR6776S"
   secret_key = "m5Mhvppw8DbvA8zcBmY+h979a/a8Vjrrwe/jh0zU"
-
   region     = "us-east-1"
 }
 
 resource "aws_instance" "Demo" {
   ami           = "ami-08a52ddb321b32a8c"
   instance_type = "t2.micro"
-  
   }
 
 ###########ssssss
