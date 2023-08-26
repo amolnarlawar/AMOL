162471c80ea127cc795204f277b19f3009ceeb64
amolnarlawar <91715308+amolnarlawar@users.noreply.github.com>
2023-08-26 15:01:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  access_key = "AKIASY5UYM73J33H2HZ3"
-  secret_key = "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"
+ # access_key = "AKIASY5UYM73J33H2HZ3"
+  # secret_key = "iXyJ6LGlQk30R3Hf6PRUK5oO4KPPpM+6cZwFO18J"
   region     = "us-east-1"
 }
 
 resource "aws_instance" "example_instance" {
-  ami           = "ami-08a52ddb321b32a8c"  # Replace with your desired AMI ID
+  ami           = "ami-051f7e7f6c2f40dc1"  # Replace with your desired AMI ID
   instance_type = "t2.micro"
 
   tags = {
