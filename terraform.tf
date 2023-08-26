c844463e591819f2e9eee061fd5b049d9873cba9
amolnarlawar <91715308+amolnarlawar@users.noreply.github.com>
2023-08-26 15:14:55 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,11 @@
- cloud {
+ terraform {
     organization = "amol-demo"
-/*
+
     workspaces {
       name = "AMOL"
     }
   }
-*/
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
